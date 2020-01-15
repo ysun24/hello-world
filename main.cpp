588bf4a5f1a026793be2babd4ca91aa951bd5e62
ysun24 <59858271+ysun24@users.noreply.github.com>
2020-01-15 15:51:11 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+// Test using GITHUB
+// Author: Yicheng Sun and 
+// Description: Hello World in C++//============================================================main
+
+#include <iostream>
+using namespace std;
+int hello1Function(string name1);
+int hello2Function(string name2);
+int main()
+{
+    string name1="Yicheng Sun";
+    hello1Function(name1);
+    string name2="your name2";
+    hello2Function(name2);
+    return 0;
+}
